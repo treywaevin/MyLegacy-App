b7fef965696d616b24e1426e10699dea81b04062
Aevin Eliares <aevin.eliares@gmail.com>
2022-07-23 00:32:33 -0700

    basic ui design

--- MyLegacy/HomeScreen.swift
+++ MyLegacy/HomeScreen.swift
@@ -7,16 +7,67 @@
 
 import SwiftUI
 
+struct Home: View{
+    @AppStorage("name") var name = "Aevin"
+    @AppStorage("current") var current = "Rest"
+    @State var dateString = ""
+    
+    var body: some View{
+        NavigationView{
+            ZStack{
+                Color(red: 217/255, green: 255/255, blue: 245/255)
+                    .ignoresSafeArea()
+                VStack(spacing: 60){
+                    // Current Workout Day
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(Color(red: 29/255, green: 30/255, blue: 24/255))
+                            .frame(width: 350, height: 150)
+                        VStack(alignment: .leading){
+                            Text("\(dateString)")
+                                .foregroundColor(.white)
+                                .font(.system(size: 30, weight: .bold, design:.rounded))
+                            Text("Today is \(current)")
+                                .foregroundColor(.white)
+                        }
+                    }
+                    
+                    // Exercise List
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(.white)
+                        LazyVStack{
+                            Text("Test")
+                            Text("Test")
+                        }
+                    }
+                }
+                    .navigationTitle("Hi \(name)!")
+            }
+        }
+        .onAppear{
+            dateString = Date.now.formatted(.dateTime.weekday(.wide).month(.wide).day())
+        }
+    }
+}
+
 struct HomeScreen: View {
     var body: some View {
         TabView{
-            Text("Home")
+            Home()
                 .tabItem {
+                    Image(systemName: "house")
                     Text("Home")
                 }
-            Text("Profile")
+            Text("Workouts")
                 .tabItem {
-                    Text("Profile")
+                    Image(systemName: "figure.walk")
+                    Text("Workout")
+                }
+            Text("Progress")
+                .tabItem {
+                    Image(systemName: "heart.text.square")
+                    Text("Progress")
                 }
         }
     }
@@ -27,3 +78,4 @@ struct HomeScreen_Previews: PreviewProvider {
         HomeScreen()
     }
 }
+
